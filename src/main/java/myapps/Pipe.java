baa4ecbd556eda2129e7b53bd4d5dc42d26416c4
tatamiya <tamyuji1002@gmail.com>
2022-03-13 11:23:04 +0900

    Writing a first Streams application: Pipe

---
+++ src/main/java/myapps/Pipe.java
@@ -0,0 +1,32 @@
+package myapps;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+
+
+
+public class Pipe {
+
+    public static void main(String[] args) throws Exception {
+        Properties props = New Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> source = builder.stream("streams-plaintext-input");
+        source.to("streams-pipe-output");
+
+        final Topology topology = builder.build();
+        System.out.println(topology.describe());
+
+
+
+    }
+
+}
