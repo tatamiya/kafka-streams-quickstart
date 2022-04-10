2acab8fb28102a8f5f9a2d294101b857775948b3
tatamiya <tamyuji1002@gmail.com>
2022-04-10 13:57:30 +0900

    fix a typo

--- src/main/java/myapps/Pipe.java
+++ src/main/java/myapps/Pipe.java
@@ -11,7 +11,7 @@ import org.apache.kafka.streams.kstream.KStream;
 public class Pipe {
 
     public static void main(String[] args) throws Exception {
-        Properties props = New Properties();
+        Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
